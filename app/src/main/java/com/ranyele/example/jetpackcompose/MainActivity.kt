2ff9607224b3f372146b18ffa946402ff9760f36
Ranyele Amorim <>
2021-06-05 23:45:53 -0300

    Add Scaffold

--- app/src/main/java/com/ranyele/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/ranyele/example/jetpackcompose/MainActivity.kt
@@ -8,4 +8,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    @Composable
+    fun NotesScreen(viewModel: MainViewModel) {
+        // Observing notes state from MainViewModel
+        ...
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = "JetNotes",
+                    icon = Icons.Filled.List, onIconClick = {}
+                ) },
+            content = {
+                if (notes.isNotEmpty()) {
+                    NotesList(
+                        notes = notes,
+                        onNoteCheckedChange = {
+                            viewModel.onNoteCheckedChange(it) },
+                        onNoteClick = { viewModel.onNoteClick(it) } )
+                } }
+        ) }
 }
